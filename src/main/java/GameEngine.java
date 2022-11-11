b315c712ab1de64a4c65d641dd1b6fb6b83739c3
AmrElkhatieb <94713322+AmrElkhatieb@users.noreply.github.com>
2022-11-11 06:18:54 +0200

    Add files via upload

--- src/main/java/GameEngine.java
+++ src/main/java/GameEngine.java
@@ -1,11 +1,17 @@
 public class GameEngine {
     public static void main(String[] args) {
-        Minimax minimax = new Minimax(2);
+        Minimax minimax = new Minimax(4);
+        MiniMaxPruning minimaxPruning = new MiniMaxPruning(8);
         State initialState = new State(0L);
         long[] info = minimax.getInfo(initialState);
+        long[] info1 = minimaxPruning.getInfo(initialState);
         System.out.println("time taken: " + info[0]);
         System.out.println("nodes expanded: " + info[1]);
         System.out.println("next play: " + minimax.getNextState(initialState).toString());
-        minimax.printGameTree(initialState);
+        System.out.println("time taken: " + info1[0]);
+        System.out.println("nodes expanded: " + info1[1]);
+        System.out.println("next play: " + minimaxPruning.getNextState(initialState).toString());
+        System.out.println(minimax.getNextState(initialState).getValue());
+        System.out.println(minimaxPruning.getNextState(initialState).getValue());
     }
 }
